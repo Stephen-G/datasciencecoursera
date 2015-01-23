4c8e25bfa17abfe85f963c80a1503a7446af45c1
Nexen-StephenG <stephen.grant@nexencnoocltd.com>
2015-01-23 13:34:57 -0700

    Empty Folder

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## This function will take a matrix, calculate the inverse and cache the inverse matrix for later use.
+
+## This function sets and caches the matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+
+}
+
+
+## This function will check the cache for the inverse matrix, retrieve if there, or calculate inverse.
+
+cacheSolve <- function(x, ...) {
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
+}
